1f925811123931073edcf941e23afb481fabdbb0
chaseDreamer90s <18842495517@139.com>
2023-04-06 21:35:41 +0800

    测试

---
+++ src/main/java/com/dixn/dxboot/TestBranch.java
@@ -0,0 +1,14 @@
+package com.dixn.dxboot;
+
+/**
+ * @ClassName TestBranch
+ * @Description TODO
+ * @Author chunyu.huang
+ * @Date 2023/4/6 21:34
+ * @Version 1.0
+ **/
+public class TestBranch {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
